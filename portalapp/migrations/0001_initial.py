5cfe860deceddf1c86a18c259844c8f47bdd40d3
Rushi Agrawal <rushi.agr@gmail.com>
2015-01-26 14:40:01 +0530

    Add DB to portalapp, and register app with project

---
+++ portalapp/migrations/0001_initial.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='UserInfo',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('username', models.CharField(max_length=255)),
+                ('password', models.CharField(max_length=255)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- portalapp/models.py
+++ portalapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class UserInfo(models.Model):
+    username = models.CharField(max_length=255)
+    # TODO(rushiagr): password not secret. Make it
+    password = models.CharField(max_length=255)
