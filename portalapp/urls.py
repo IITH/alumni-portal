35a524baaa2a6f2ff6d3bd7ceafdddedbbc76691
Chirag Choudhary <chirag200666@gmail.com>
2015-03-22 23:58:03 +0530

    simple name search api & search page using twitter typehead.

--- portalapp/urls.py
+++ portalapp/urls.py
@@ -7,6 +7,8 @@ urlpatterns = patterns(
     url(r'^$', views.index, name='index'),
     url(r'^profile/$', views.view_userinfo, name='view_userinfo'),
     url(r'^profile/create$', views.create_userinfo, name='create_userinfo'),
+    url(r'^search/$', views.search, name='search'),
+    url(r'^searchname$', views.search_name, name='search_name'),
     url(r'^profile/update/(?P<pk>\d+)$', views.UserInfoUpdate.as_view(success_url=reverse_lazy('portalapp:view_userinfo')), name='update_userinfo'),
 
 )
